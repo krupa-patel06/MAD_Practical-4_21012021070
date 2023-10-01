c1d134725f1987c59703487a9d6073a22a68ef71
Divy484 <98522523+Divy484@users.noreply.github.com>
2023-10-01 21:20:16 +0530

    Practical 4

---
+++ app/src/main/java/com/example/mad_practical_4_21012021070/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mad_practical_4_21012021070
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val login: Button = findViewById(R.id.login)
+        login.setOnClickListener {
+            Intent(this,LoginActivity::class.java).also { startActivity(it) }
+        }
+
+        val signup: Button = findViewById(R.id.signup)
+        signup.setOnClickListener {
+            Intent(this,RegistrationActivity::class.java).also { startActivity(it) }
+        }
+    }
+}
